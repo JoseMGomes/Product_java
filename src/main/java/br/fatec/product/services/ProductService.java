3518d962ff093be5667317469f1c8bed3de04638
José Lucas <gomessze03@gmail.com>
2024-11-12 13:16:47 -0300

    PUT, POST and DELETE function

--- src/main/java/br/fatec/product/services/ProductService.java
+++ src/main/java/br/fatec/product/services/ProductService.java
@@ -15,14 +15,34 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return repository.findAll();
 
     }
-    
-    public Product getProductById(long id){
+
+    public Product getProductById(long id) {
         return repository.findById(id).orElseThrow(
-            () -> new EntityNotFoundException("Produto não cadastrado")
-        );
+                () -> new EntityNotFoundException("Produto não cadastrado"));
+    }
+
+    public void delete(long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Produto não cadastrado");
+        }
+    }
+
+    public Product save(Product product) {
+        return repository.save(product);
+    }
+
+    public void update(Product product, long id) {
+        Product aux = repository.getReferenceById(id);
+        aux.setCategory(product.getCategory());
+        aux.setName(product.getName());
+        aux.setPrice(product.getPrice());
+
+        repository.save(aux);
     }
 }
